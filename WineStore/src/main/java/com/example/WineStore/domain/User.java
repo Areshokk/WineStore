7e350642eef6a0f0395e92b30f78f47171c99d0a
Yura <yrameda0404@gmail.com>
2022-04-14 12:14:35 +0300

    added entity, header, footer

---
+++ WineStore/src/main/java/com/example/WineStore/domain/User.java
@@ -0,0 +1,78 @@
+package com.example.WineStore.domain;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "usr")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id", "username", "activationCode"})
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String email;
+
+    private boolean active;
+
+    @Column(name = "activation_code")
+    private String activationCode;
+
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Wine> perfumeList;
+
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+}
---
+++ WineStore/src/test/java/com/example/WineStore/WineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WineStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
